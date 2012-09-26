49669b4d533df17e439dd9b98351b9ded7e11e1c
Francisco Roque <francisco@roque.com.pt>
2012-09-26 15:41:28 +0200

    Fixed error when uploading image with single tag in r1.9

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -67,7 +67,7 @@ class Photo < ActiveRecord::Base
     self.latitude = photo.GPSLatitude if self.latitude.nil?
     self.title = photo.DocumentName if self.title.nil?
     self.description = photo.ImageDescription if self.description.nil? && photo.ImageDescription != 'Exif_JPEG_PICTURE'
-    self.tag_list = (self.tags.empty? ? "" : self.album.tag_list) + " " + (photo.Keywords.nil? ? "" : photo.Keywords.to_a.map { |tag| tag.gsub(" ", "_") }.join(" "))
+    self.tag_list = (self.tags.empty? ? "" : self.album.tag_list) + " " + (photo.Keywords.nil? ? "" : Array(photo.Keywords).map { |tag| tag.gsub(" ", "_") }.join(" "))
   end
   
   def exif_write
